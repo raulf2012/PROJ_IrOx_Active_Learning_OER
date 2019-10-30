e6f768ff67d171e392c72324a76630118e54d815
Raul Flores <raulf2012@gmail.com>
2019-10-30 16:12:52 -0700

    CCF notebooks

--- workflow/test_ccf_code_chuanxun/test_code.py
+++ workflow/test_ccf_code_chuanxun/test_code.py
@@ -6,18 +6,31 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.4'
-#       jupytext_version: 1.1.7
+#       jupytext_version: 1.2.1
 #   kernelspec:
-#     display_name: Python [conda env:research-new]
+#     display_name: Python [conda env:PROJ_IrOx_Active_Learning_OER]
 #     language: python
-#     name: conda-env-research-new-py
+#     name: conda-env-PROJ_IrOx_Active_Learning_OER-py
 # ---
+
+# # Import Modules
 
 # + {"jupyter": {"source_hidden": true}}
 import os
 import sys
+import copy
 
 import pickle
+
+import numpy as np
+
+import pandas as pd
+
+from ase.visualize import view
+
+# Plotly
+import chart_studio.plotly as py
+import plotly.graph_objs as go
 
 
 # #############################################################################
@@ -46,26 +59,122 @@ from proj_data_irox import (
     )
 # -
 
+# # Script Inputs
+
+# +
+r_cut_off = 10.
+
+r_vector = np.arange(1, 10, 0.02)
+# -
+
 # # Read Data
 
+# +
 with open(static_irox_structures_path, "rb") as fle:
     df_structures = pickle.load(fle)
 
+atoms_0 = df_structures.iloc[0]["atoms"]
+
+# atoms_1 = df_structures.iloc[50]["atoms"]
+
+atoms_1 = copy.deepcopy(atoms_0)
+atoms_1.rattle(
+    stdev=0.2,
+#     stdev=0.01,
+    )
+# -
+
+assert False
+
+
+def get_ccf_df(ccf, r_cut_off, r_vector):
+    """
+    """
+    df_i = pd.DataFrame()
+    for key, value in ccf.items():
+        df_i[key] = value
+
+    df_i = df_i.set_index(r_vector)
+    
+    return(df_i)
+
+
 # +
-atoms_i = df_structures.iloc[0]["atoms"]
+ccf_0 = struc2ccf(atoms_0, r_cut_off, r_vector)
+ccf_1 = struc2ccf(atoms_1, r_cut_off, r_vector)
 
-atoms_i
+df_0 = get_ccf_df(ccf_0, r_cut_off, r_vector)
+df_1 = get_ccf_df(ccf_1, r_cut_off, r_vector)
+# -
+
+cal_ccf_d(ccf_0, ccf_1)
 
 # +
+data = []
+df_i = df_0
+for col_i in list(df_i):
+    trace_i = go.Scatter(
+        x=list(df_i.index),
+        y=df_i[col_i],
+        mode="lines+markers",
 
-r_cut_off = 5.
-# r_vector = [1., 5.,]
-r_vector = 3.
+        line=dict(
+            color='red',
+            width=0.8,
+            ),
+
+        )
+    data.append(trace_i)
+
+df_i = df_1
+for col_i in list(df_i):
+    trace_i = go.Scatter(
+        x=list(df_i.index),
+        y=df_i[col_i],
+        mode="lines+markers",
+
+        line=dict(
+            color='black',
+            width=0.8,
+            ),
+
+        )
+    data.append(trace_i)
+
+fig = go.Figure(data=data)
+fig.show()
+
+# + {"active": ""}
+#
+#
+#
+
+# + {"jupyter": {"source_hidden": true}}
+# ccf_0 = struc2ccf(atoms_0, r_cut_off, r_vector)
+# ccf_50 = struc2ccf(atoms_50, r_cut_off, r_vector)
 
 
+# cal_ccf_d(ccf_0, ccf_50)
 
-struc2ccf(atoms_i, 1, [1, 2])
+# # i_a_d =cal_inter_atomic_d(atoms_i, 5.)
+# # d2ccf(i_a_d, r_cut_off, r_vector)
 
+# ccf_0_list = []
+# ccf_1_list = []
 
-i_a_d =cal_inter_atomic_d(atoms_i, 5.)
-d2ccf(i_a_d, r_cut_off, r_vector)
+# r_vector_array = np.arange(1, 10, 2.)
+# for r_vector in r_vector_array:
+#     print(r_vector)
+
+#     ccf_0 = struc2ccf(atoms_0, r_cut_off, r_vector)
+#     ccf_1 = struc2ccf(atoms_1, r_cut_off, r_vector)
+
+#     ccf_0_list.append(ccf_0)
+#     ccf_1_list.append(ccf_1)
+
+# df_0 = pd.DataFrame(
+#     ccf_0_list
+#     )
+
+# ccf_0 = struc2ccf(atoms_0, r_cut_off, r_vector)
+# ccf_1 = struc2ccf(atoms_1, r_cut_off, r_vector)
