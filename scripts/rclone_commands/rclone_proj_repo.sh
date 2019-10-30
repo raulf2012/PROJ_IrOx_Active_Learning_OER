c709f4497b3323647783246c4cf5995930e86c41
Raul Flores <raulf2012@gmail.com>
2019-10-30 16:10:07 -0700

    Updates to PROJ scripts

--- scripts/rclone_commands/rclone_proj_repo.sh
+++ scripts/rclone_commands/rclone_proj_repo.sh
@@ -4,7 +4,7 @@ rclone --transfers=2 sync \
 raul_dropbox:01_norskov/00_git_repos/PROJ_IrOx_Active_Learning_OER \
 $norskov_research/00_git_repos/PROJ_IrOx_Active_Learning_OER \
 --exclude ".git/**" \
---exclude "voro_temp" \
+--exclude "*voro_temp*" \
 --verbose \
   
 
