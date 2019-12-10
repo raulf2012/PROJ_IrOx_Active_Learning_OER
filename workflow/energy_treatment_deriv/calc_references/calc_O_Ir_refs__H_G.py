d6f4698680c447646e845af6a82801d47c9e6832
Raul Flores <raulf2012@gmail.com>
2019-12-09 20:59:51 -0800

    Updates to energy treatment derivation

---
+++ workflow/energy_treatment_deriv/calc_references/calc_O_Ir_refs__H_G.py
@@ -0,0 +1,217 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.1
+#   kernelspec:
+#     display_name: Python [conda env:research-new] *
+#     language: python
+#     name: conda-env-research-new-py
+# ---
+
+# # Bulk Energy Treatment for IrOx Systems
+# ---
+
+# # Import Modules
+
+# +
+import os
+import sys
+
+sys.path.insert(0,
+    os.path.join(
+        os.environ["PROJ_irox"],
+        "data"))
+
+# #############################################################################
+from energetics.dft_energy import Element_Refs
+
+# #############################################################################
+# Gas phase molecules
+from proj_data_irox import (
+    zpe_h2o,
+    cv_h2o,
+    ts_h2o,
+
+    zpe_h2,
+    cv_h2,
+    ts_h2)
+
+# #############################################################################
+from proj_data_irox import gas_dft_references_dict
+gas_dft_refs_i = gas_dft_references_dict[500]
+
+h2o_ref = gas_dft_refs_i["h2o"]
+h2_ref = gas_dft_refs_i["h2"]
+# -
+
+# # Script Inputs
+
+# +
+T = 298.15
+
+# DFT Quantities
+dft_energy_dict = {
+    # FROM AL calculations at 500 eV
+    "iro2": -7.047516,
+    # "iro2": -7.047426,
+
+    "iro3": -6.469847,
+    # "iro3": -6.467450,
+    }
+
+
+# #############################################################################
+# IrO2 Experimental thermochemical data #######################################
+
+# dh_iro2_exp = -242.672 / kjmol  # Barin
+dh_iro2_exp = -2.515126703632689
+
+# dg_iro2_exp = -188.386 / kjmol  # Barin
+dg_iro2_exp = -1.9524900243561174
+
+
+# #############################################################################
+# Entropies ###################################################################
+
+# Reference for IrO2 thermochemical data
+# https://onlinelibrary.wiley.com/doi/book/10.1002/9783527619825
+# Thermochemical Data of Pure Substances, Third Edition
+
+# rutile-IrO2
+S_iro2_solid = 58.576  # J/(mol K)  # Barin
+TS_iro2_solid = 0.18098818987407367  # eV
+
+
+# Ir metal
+S_ir_metal = 35.505  # J/(mol K)  # Barin
+TS_ir_metal = 0.10969917603772607  # eV
+
+
+# Oxygen gas entropy reference
+# https://webbook.nist.gov/cgi/cbook.cgi?ID=C7782447&Mask=1
+# CODATA key values for thermodynamics
+# https://www.worldcat.org/title/codata-key-values-for-thermodynamics/oclc/18559968
+S_o2_gas = 205.2  # J/(mol K)  # CODATA thermo book
+TS_o2_gas = 0.6340921386744053  # eV
+# -
+
+# # Gas Phase References
+
+# +
+# # %%capture
+
+e_h2o_r = h2o_ref
+e_h2_r = h2_ref
+
+h_h2o_r = e_h2o_r + zpe_h2o + cv_h2o
+h_h2_r = e_h2_r + zpe_h2 + cv_h2
+
+g_h2o_r = h_h2o_r + ts_h2o
+g_h2_r = h_h2_r + ts_h2
+
+# #############################################################################
+Elem_Refs = Element_Refs(
+    H2O_dict={
+        "electronic_e": e_h2o_r, "enthalpy_e": h_h2o_r, "gibbs_e": g_h2o_r},
+    H2_dict={
+        "electronic_e": e_h2_r, "enthalpy_e": h_h2_r, "gibbs_e": g_h2_r},
+    oxygen_ref="O2",
+    hydrogen_ref="H2")
+
+oxy_ref, hyd_ref = Elem_Refs.calc_ref_energies()
+# -
+
+# # Fitting the Ir metal reference to the experimental formation energy of IrO2
+
+# +
+e_iro2_pa = dft_energy_dict["iro2"]
+e_iro2 = 3 * e_iro2_pa
+
+PV_i = 0.
+h_iro2 = e_iro2 + PV_i
+
+oxy_ref_h = oxy_ref.enthalpy_e
+
+h_ir_m_fit = h_iro2 - (2 * oxy_ref_h) - dh_iro2_exp  # fit to exp dH
+
+dh_iro2 = h_iro2 - (2 * oxy_ref.enthalpy_e + h_ir_m_fit)
+
+# +
+e_iro3_pa = dft_energy_dict["iro3"]
+e_iro3 = 4 * e_iro3_pa
+
+PV_i = 0.
+h_iro3 = e_iro3 + PV_i
+
+dh_iro3 = h_iro3 - (3 * oxy_ref.enthalpy_e + h_ir_m_fit)
+# -
+
+# # Calculate Gibbs Free Energy
+
+# +
+TdS_iro2 = TS_iro2_solid - TS_ir_metal - TS_o2_gas
+
+# Alternate calculate of TdS for IrO2
+# TdS_iro2 = dh_iro2_exp - dg_iro2_exp
+
+dg_iro2 = dh_iro2 - TdS_iro2
+# -
+
+# # Calculating Gibbs free energy of IrO3 polymorph
+
+# +
+# Entropy for WOx from Barrin tables
+# WO3: 75.898
+# WO2: 50.543
+
+factor = 75.898 / 50.543
+
+# adjusted to reflect more O in IrO3 vs IrO2
+TdS_iro3 = factor * TS_iro2_solid - TS_ir_metal - 3 / 2 * TS_o2_gas
+
+# +
+e_iro3_pa = dft_energy_dict["iro3"]
+e_iro3 = 4 * e_iro3_pa
+
+PV_i = 0.
+h_iro3 = e_iro3 + PV_i
+
+dh_iro3 = h_iro3 - (3 * oxy_ref.enthalpy_e + h_ir_m_fit)
+dg_iro3 = dh_iro3 - TdS_iro3
+
+# + {"active": ""}
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+# +
+print(40 * "-")
+print("O ref:", oxy_ref.enthalpy_e)
+print("Ir ref:", h_ir_m_fit)
+
+print(40 * "-")
+print("TdS_iro2:", TdS_iro2)
+print("TdS_iro3:", TdS_iro3)
+
+print(40 * "-")
+print("dh_iro2:", dh_iro2)
+print("dh_iro3:", dh_iro3)
+print("")
+print("dg_iro2:", dg_iro2)
+print("dg_iro3:", dg_iro3)
+print("")
+print("dh_iro2 - dh_iro3:", dh_iro2 - dh_iro3)
+print("dg_iro2 - dg_iro3:", dg_iro2 - dg_iro3)
+
+print(40 * "-")
